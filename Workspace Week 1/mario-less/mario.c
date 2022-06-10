f91ba4536459899678f1560aaf5c7f8bb61c40f5
Adam Salem <84548368+ItzVoidYT@users.noreply.github.com>
2022-06-09 21:39:47 -0400

    Add files via upload

---
+++ Workspace Week 1/mario-less/mario.c
@@ -0,0 +1,30 @@
+#include <cs50.h>
+#include <stdio.h>
+
+
+void makePyramid(int height);
+
+int main(void) {
+    int height;
+    do {
+        height = get_int("Enter the height of the pyramid: ");
+    }
+
+    while(height <= 0 || height > 8);
+
+    makePyramid(height);
+
+}
+
+void makePyramid(int height) {
+    for (int i = 1; i <= height; i++) {
+        for (int j = i; j < height; j++) {
+            printf(" ");
+        }
+
+        for (int k = 1; k <= i; k++) {
+            printf("#");
+        }
+        printf("\n");
+    }
+}

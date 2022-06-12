eac1f5aa9cf27ddefca59cc4ce3a00064e2ef970
Adam Salem <84548368+ItzVoidYT@users.noreply.github.com>
2022-06-12 11:40:03 -0400

    Add files via upload

--- Workspace Week 1/mario-less/mario.c
+++ Workspace Week 1/mario-less/mario.c
@@ -4,25 +4,30 @@
 
 void makePyramid(int height);
 
-int main(void) {
-    int height;
-    do {
-        height = get_int("Enter the height of the pyramid: ");
+int main(void)
+{
+    int height; // Declares int height to be used outside of the do's scope
+    do
+    {
+        height = get_int("Enter the height of the pyramid: "); // Gets user input (Height)
     }
+    while (height <= 0 || height > 8);
 
-    while(height <= 0 || height > 8);
-
-    makePyramid(height);
+    makePyramid(height); // Makes the pyramid...
 
 }
 
-void makePyramid(int height) {
-    for (int i = 1; i <= height; i++) {
-        for (int j = i; j < height; j++) {
+void makePyramid(int height)
+{
+    for (int i = 1; i <= height; i++)
+    {
+        for (int j = i; j < height; j++)
+        {
             printf(" ");
         }
 
-        for (int k = 1; k <= i; k++) {
+        for (int k = 1; k <= i; k++)
+        {
             printf("#");
         }
         printf("\n");

6ff4278e11c8ebfa75bcfa7125d7b6d159a4fc8c
Adam Salem <84548368+ItzVoidYT@users.noreply.github.com>
2022-06-12 11:40:49 -0400

    Add files via upload

--- Workspace Week 1/mario-more/mario.c
+++ Workspace Week 1/mario-more/mario.c
@@ -5,37 +5,46 @@
 void makePyramid(int height);
 
 
-int main(void) {
-    int height;
-    do {
-        height = get_int("Enter the height of the pyramid.");
+int main(void)
+{
+    int height; // Declares height so you can use it outside of the scope of the do loop.
+
+    do
+    {
+        height = get_int("Enter the height of the pyramid."); // Gets user input (Height)
     }
-    while(height <= 0 || height > 8);
-    makePyramid(height);
+    while (height <= 0 || height > 8); // Re-runs the loop if height isn't less than or equal to 0.
+
+    makePyramid(height); // Makes the pyramid (Calls makePyramid function).
 
 }
 
 
 
 
-void makePyramid(int height) {
+void makePyramid(int height)
+{
 
-    for (int i = 1; i <= height; i++) {
+    for (int i = 1; i <= height; i++)
+    {
 
-        for (int j = i; j < height; j++) {
+        for (int j = i; j < height; j++)
+        {
             printf(" ");
         }
 
-        for (int k = 1; k <= i; k++) {
+        for (int k = 1; k <= i; k++)
+        {
             printf("#");
         }
         printf("  ");
 
 
-        for(int y = 1; y <= i; y++) {
+        for (int y = 1; y <= i; y++)
+        {
             printf("#");
         }
-        printf("\n");
+        printf("\n");// Adds new layer to pyramid. (newline)
 
     }
 

a5d36f7c454527c908752963ea75d409acb35a5f
Adam Salem <84548368+ItzVoidYT@users.noreply.github.com>
2022-06-12 11:40:33 -0400

    Add files via upload

--- Workspace Week 1/hello/hello.c
+++ Workspace Week 1/hello/hello.c
@@ -2,9 +2,10 @@
 #include <cs50.h>
 
 
-int main(void) {
-    string name = get_string("What's your name? ");
+int main(void)
+{
+    string name = get_string("What's your name? "); // Asks your name and stores it in a variable
 
-    printf("Hello, %s\n", name);
+    printf("Hello, %s\n", name); // Says hello (Your name).
 
 }
